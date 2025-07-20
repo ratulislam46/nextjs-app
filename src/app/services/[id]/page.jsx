524b7c274e3f02bc9ef451befe7a86f4b1dde733
ratulislam46 <arfanratul46@gmail.com>
2025-07-20 11:09:30 +0600

    dynamic route and nested route

---
+++ src/app/services/[id]/page.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+const ServiceDetailsPae = ({ params }) => {
+
+    const data = [
+        {
+            _id: 1,
+            divition: "Barishal",
+            city: "Barishal"
+        },
+        {
+            _id: 2,
+            divition: "Barishal",
+            city: "Potuakhali"
+        },
+        {
+            _id: 3,
+            divition: "Dhaka",
+            city: "Mirpur"
+        },
+        {
+            _id: 4,
+            divition: "Sylhet",
+            city: "Hemayetpur"
+        }
+    ];
+
+    const id = params.id;
+    const filterData = data.find((d) => d._id == id);
+    console.log(filterData);
+
+    return (
+        <div>
+            <h1>Services Details page</h1>
+            <p>ID: {params.id}</p>
+            <h3>Devition: {filterData.divition}</h3>
+            <h3>City: {filterData.city}</h3>
+        </div>
+    );
+};
+
+export default ServiceDetailsPae;
--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,9 +1,45 @@
+import Link from 'next/link';
 import React from 'react';
+
+const data = [
+    {
+        _id: 1,
+        divition: "Barishal",
+        city: "Barishal"
+    },
+    {
+        _id: 2,
+        divition: "Barishal",
+        city: "Potuakhali"
+    },
+    {
+        _id: 3,
+        divition: "Dhaka",
+        city: "Mirpur"
+    },
+    {
+        _id: 4,
+        divition: "Sylhet",
+        city: "Hemayetpur"
+    }
+]
 
 const Services = () => {
     return (
         <div>
             <p className='font-bold text-3xl'>Services page</p>
+
+            {
+                data.map(d => {
+                    return (
+                        <div key={d._id}>
+                            <Link href={`/services/${d._id}`}>
+                                <p className='border p-2 mb-4'>{d.city}</p>
+                            </Link>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 };
